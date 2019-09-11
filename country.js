9d013fb301c962486e1a182a66367637d69a9260
Chamunorwa Mutezva <ckmutezva@gmail.com>
2019-09-11 22:16:25 +0200

    mobile responsive

--- country.js
+++ country.js
@@ -1,5 +1,10 @@
 const country = document.querySelector(".country");
+const btn = document.getElementById("backwards");
 console.log(country);
+console.log(btn);
+btn.addEventListener("click", function () {
+    window.location = "index.html";
+})
 
 const fetchOneCountry = () => {
     const apiEndpoint = `https://restcountries.eu/rest/v2/callingcode/372`;
